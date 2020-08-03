cfa6302ae53a3793a4c6dd3740191e15b101cef3
manizha01 <manizha.razban@gmail.com>
2020-08-02 21:55:01 -0400

    first commit

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+
 release: python manage.py migrate
-web: gunicorn Razban.wsgi --log-file -
+web: gunicorn razban.wsgi --log-file -
--- Razban/wsgi.py
+++ Razban/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Razban project.
+WSGI config for razban project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Razban.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'razban.settings')
 
 application = get_wsgi_application()
